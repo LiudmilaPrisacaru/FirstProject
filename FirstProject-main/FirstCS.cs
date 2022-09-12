1bb59160c1480506cd969f5ed57d867e06ac544f
Liudmila Prisacaru <liudmila.prisacaru@amdaris.com>
2022-09-12 09:36:08 +0300

    FirstCS committed

---
+++ FirstProject-main/FirstCS.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace LP_MyApplication
+{
+    class LP_Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            int myAge = 40;
+            Console.WriteLine("I'm " + myAge + " years old.");
+
+            const string currentMonth = "September";
+            Console.WriteLine(currentMonth + " is the most beautiful month of the year.");
+
+            Console.WriteLine("Enter date:");
+            int date = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Today is the " + date + "th of september.");
+        }
+    }
+}
